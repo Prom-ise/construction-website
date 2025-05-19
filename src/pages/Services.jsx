149f1f9567d3868709996f8688687dc2d30a78d4
promise <emmanuelpromise456@gmail.com>
2025-05-19 15:22:38 +0100

    Refactors styling in Services component
    Updates the styling in the Services component for improved readability and consistency. Removes redundant code and adjusts the layout of featured services sections.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import construct7 from '../assets/construct7.jpg'
+import React from "react";
+import construct7 from "../assets/construct7.jpg";
 
 const Services = () => {
   return (
     <div>
       <div
-              className="w-full bg-cover p-[4em] bg-center bg-no-repeat relative"
-              style={{ backgroundImage: `url(${construct7})` }}
-            >
-              <div className="absolute inset-0 bg-black/60"></div>
-              <div className="relative z-10">
-                <h1 className="text-6xl font-bold text-start text-white">
-                  Services
-                </h1>
-                <h3>Construction services</h3>
-              </div>
-            </div>
+        className="w-full bg-cover p-[4em] bg-center bg-no-repeat relative"
+        style={{ backgroundImage: `url(${construct7})` }}
+      >
+        <div className="absolute inset-0 bg-black/60"></div>
+        <div className="relative z-10">
+          <h1 className="text-6xl font-bold text-start text-white">Services</h1>
+          <h3>Construction services</h3>
+        </div>
+      </div>
 
-            <div>
-              <h1 className='text-5xl font-bold text-[rgb(254,93,20)] mt-[4em] mb-[4em]'>Our Featured Services</h1>
-              <p>NIA Construction tackles all projects with a personal commitment to providing an efficient and professional experience from start to finish. We understand the stress our clients experience when selecting the right company for their real estate related projects. Whether the project is building a new home, remodeling an existing one or assisting with a maintenance project, NIA Construction approaches every project with the utmost integrity.</p>
+      <div className="container mx-auto ms-4 my-[10em]">
+        <h1 className="text-2xl font-bold text-black ">
+          Our Featured Services
+        </h1>
+        <p className="text-black my-[1em]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dicta excepturi aut, harum numquam unde aperiam, voluptate tenetur minima commodi qui, voluptates eaque vitae eos reiciendis sapiente iusto vel maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis est fugiat obcaecati consequatur illo natus soluta dolorum harum voluptates iusto quas, alias dignissimos commodi perferendis et veritatis distinctio sint. Ratione?
+        </p>
 
-<p>We take the time to fully realize the needs of our clients so that we can work as a unified front to achieve the best possible result. We live by the motto that no job is too small or robust, and take pride in understanding and servicing the needs of the client first. We aim to not only meet, but to exceed deliverable expectations. We look forward to "building" lasting relationships and to earning your trust and business!</p>
-            </div>
+        <p className="text-black">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro error impedit assumenda aspernatur harum voluptates voluptatum, consectetur quasi at cum doloribus laborum totam officiis similique reiciendis vero, reprehenderit atque iure? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum itaque iure deserunt ducimus adipisci, perspiciatis non autem maiores nulla blanditiis quae sed animi dicta cum enim quo possimus. Eum, corporis.
+        </p>
+      </div>
+
+      <div className="mx-auto">
+        <h1 className="text-3xl mt-[2em] font-bold text-center text-[rgb(254,93,20)]">Featured Construction Services</h1>
+        <div>
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
