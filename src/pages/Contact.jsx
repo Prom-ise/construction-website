4636c6d5ef339442c9b772bed3d289365a99272d
promise <emmanuelpromise456@gmail.com>
2025-05-19 13:07:21 +0100

    Updates input fields styling in Contact page
    Improves the visual appearance of input fields by applying consistent styling for better user experience.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,6 +31,20 @@ const Contact = () => {
           type="text"
         />
         </div>
+        <div className="flex">
+          <input
+          class="bg-[#222630] px-4 py-3 outline-none w-[280px] text-white rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+          name="tel"
+          placeholder="Contact Number"
+          type="number"
+        />
+        <input
+          class="bg-[#222630] px-4 py-3 outline-none w-[280px] text-white rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+          name="text"
+          placeholder="Enter email or username"
+          type="text"
+        />
+        </div>
       </div>
     </div>
   );
