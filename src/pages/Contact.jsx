dd8e6e98b95259b451a8bbbb96ccdda56823665f
promise <emmanuelpromise456@gmail.com>
2025-05-19 12:47:06 +0100

    refactor: remove unused input styles and update Contact component input

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,10 +14,12 @@ const Contact = () => {
       </div>
             </div>
 
-            <div className="input-groud">
-  <input required="" type="text" name="text" autocomplete="off" className="input"/>
-  <label className="user-label">First Name</label>
-</div>
+          <input
+  class="bg-[#222630] px-4 py-3 outline-none w-[280px] text-white rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040]"
+  name="text"
+  placeholder="Enter email or username"
+  type="text"
+/>
     </div>
   )
 }
